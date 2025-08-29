3872c9efc822459cc80ca918e39bb809e47ac3b2
BBS-ALERT <renatoperolalipiojr@gmail.com>
2025-08-29 12:08:26 +0400

    button working

--- alert-pwa/public/firebase-messaging-sw.js
+++ alert-pwa/public/firebase-messaging-sw.js
@@ -1,22 +1,50 @@
 // Firebase Messaging Service Worker
-importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-messaging-compat.js');
+//importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js');
+//importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-messaging-compat.js');
+
+//firebase.initializeApp({
+//  apiKey: "YOUR_API_KEY",
+//////  projectId: "YOUR_PROJECT_ID",
+//  messagingSenderId: "YOUR_SENDER_ID",
+//  appId: "YOUR_APP_ID"
+//});
+
+//const messaging = firebase.messaging();
+
+//messaging.onBackgroundMessage(function(payload) {
+//  self.registration.showNotification(
+//    payload.notification.title,
+//    {
+    //  body: payload.notification.body,
+  //    icon: '/icon-192.png'
+//   }
+//  );
+//});
+
+// public/firebase-messaging-sw.js
+importScripts('https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.22.2/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: "YOUR_API_KEY",
-  projectId: "YOUR_PROJECT_ID",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAbohcONTb1FGRjPiVxWblTc-esImwEcI8",
+  authDomain: "bbs-alert.firebaseapp.com",
+  projectId: "bbs-alert",
+  storageBucket: "bbs-alert.appspot.com",
+  messagingSenderId: "188088530343",
+  appId: "1:188088530343:web:63f5a52963276ed0cdf282",
+  measurementId: "G-3E21ZRM641"
 });
 
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+
   self.registration.showNotification(
-    payload.notification.title,
+    payload.notification?.title || "New Alert",
     {
-      body: payload.notification.body,
-      icon: '/icon-192.png'
+      body: payload.notification?.body,
+      icon: '/icons/icon-192x192.png' // make sure this file exists
     }
   );
 });
